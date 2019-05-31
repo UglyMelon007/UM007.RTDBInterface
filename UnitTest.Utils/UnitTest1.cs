a9ac6c60a363c5fff21771c1c82952a3242439f1
超 方 <UglyMelon007@163.com>
2019-05-31 17:06:24 +0800

    4

---
+++ UnitTest.Utils/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Autofac;
+using Framework.Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utils.Helper;
+
+namespace UnitTest.Utils
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IContainer _container;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _container = AutofacModule.InitTest();
+        }
+
+        [TestMethod]
+        public void RTDBTest()
+        {
+            RTDBHelper.GetDataByTagAndTime("HLU1_HLU1_FIC53101", DateTime.Now);
+        }
+    }
+}
