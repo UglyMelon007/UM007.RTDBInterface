1ea70d4d8413029448f8ac27f7d8ebe02a28483b
超 方 <UglyMelon007@163.com>
2019-05-31 16:54:58 +0800

    2

---
+++ Utils.Helper/RTDBHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using DataSource.RTDB;
+
+namespace Utils.Helper
+{
+    public static class RTDBHelper
+    {
+        public static double GetDataByTagAndTime(string tagName, DateTime dateTime)
+        {
+            return new PHD().GetDataByTagAndTime(tagName, dateTime, GetIP(tagName));
+        }
+
+        public static double[] GetDataByTagAndDuration(string tagName, DateTime startDateTime, DateTime endDateTime,
+            string tagIp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DataSet GetDataByTagsAndTime(List<string> tagList, DateTime dateTime, string tagIp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DataSet GetDataByTagsAndDuration(List<string> tagList, DateTime startDateTime,
+            DateTime endDateTime, string tagIp)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string GetIP(string tagName)
+        {
+            return "10.189.100.52";
+        }
+    }
+}
