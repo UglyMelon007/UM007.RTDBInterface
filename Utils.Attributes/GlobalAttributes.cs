ab4e0a533e836f7af87d473a645739bde1627f56
超 方 <UglyMelon007@163.com>
2019-05-31 16:27:44 +0800

    init

---
+++ Utils.Attributes/GlobalAttributes.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Utils.Helper
+{
+    public static class GlobalAttributes
+    {
+        public static string RepositoryName { get; set; }
+        public static IConfiguration Configuration { get; set; }
+    }
+}
