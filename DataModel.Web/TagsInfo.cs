227e21dccdc7d0ca3670e1b620529a34d58a1b03
超 方 <UglyMelon007@163.com>
2019-05-31 17:01:47 +0800

    2

---
+++ DataModel.Web/TagsInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace DataModel.Web
+{
+    public class TagsInfo
+    {
+        public string TagsName { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string DateTime { get; set; }
+    }
+}
---
+++ DataTransfer.RTDBWebService/Controllers/RTDBController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using DataModel.Web;
+using Microsoft.AspNetCore.Mvc;
+using Utils.Helper;
+
+namespace DataTransfer.RTDBWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RTDBController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return
+                $"Get: GetDataByTagAndTime、GetDataByTagAndDuration;\r\nPost:GetDataByTagsAndTime、GetDataByTagsAndDuration";
+        }
+
+        [HttpGet("GetDataByTagAndTime")]
+        public ActionResult<string> GetDataByTagAndTime(string tagName, string dateTime)
+        {
+            //yyyy-MM-dd hh:mm:ss
+            return RTDBHelper.GetDataByTagAndTime(tagName, Convert.ToDateTime(dateTime)).ToString(CultureInfo.CurrentCulture);
+        }
+
+        [HttpGet("GetDataByTagAndDuration")]
+        public ActionResult<string> GetDataByTagAndDuration(string tagName, string startTime, string endTime)
+        {
+            return $"GetDataByTagAndTime:\r\nTagName:{tagName}\r\nStartTime:{startTime}-EndTime:{endTime}";
+        }
+
+        [HttpPost("GetDataByTagsAndTime")]
+        public ActionResult<string> GetDataByTagsAndTime([FromBody] TagsInfo tagsInfo)
+        {
+            return $"GetDataByTagAndTime:\r\nTagName:{tagsInfo.TagsName}\r\nDateTime:{tagsInfo.DateTime}";
+        }
+
+        [HttpPost("GetDataByTagsAndDuration")]
+        public ActionResult<string> GetDataByTagsAndDuration([FromBody] TagsInfo tagsInfo)
+        {
+            return
+                $"GetDataByTagsAndDuration:\r\nTagName:{tagsInfo.TagsName}\r\nStartTime:{tagsInfo.StartTime}-EndTime:{tagsInfo.EndTime}";
+        }
+    }
+}
