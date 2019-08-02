ef8f42fd326318dd3ac22d6dcf2da0e0c2efaf10
UM007 <UglyMelon007@gmail.com>
2019-08-02 09:48:47 +0800

    原版返回格式

--- Utils.ExtensionMethods/DataSetExtension.cs
+++ Utils.ExtensionMethods/DataSetExtension.cs
@@ -33,19 +33,7 @@ namespace Utils.ExtensionMethods
 
         public static string DataFormat(this DataSet dataSet)
         {
-            var result = "";
-            DataSet ds = new DataSet();
-            DataTable dt = new DataTable(dataSet.Tables[0].Rows[0]["timeStamp"].ToString().Substring(0,16));
-            DataRow dr = dt.NewRow();
-            foreach (DataRow dataRow in dataSet.Tables[0].Rows)
-            {
-                dt.Columns.Add(dataRow["tagName"].ToString());
-                dr[dataRow["tagName"].ToString()] = dataRow["value"].ToString();
-            }
-
-            dt.Rows.Add(dr);
-            ds.Tables.Add(dt);
-            result  = JsonConvert.SerializeObject(ds).Replace("[","").Replace("]","");
+            string result  = JsonConvert.SerializeObject(dataSet);
             return result;
         }
     }
